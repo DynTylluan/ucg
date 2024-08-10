4a6b52a089ef33e890772d3aa5ccbd84dc908712
Cass <owlman@protonmail.com>
2024-08-10 18:26:21 +0100

    added c port
    Co-authored-by: Piper <piper@yarrie.net@>

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc ugc.c -o ucg
+
+run: all
+	./ucg
---
+++ ucg.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#define x printf
+int main(){x("You are dying. 1=live ");if (getchar() == '1'){x("won");}else{x("died");}}
