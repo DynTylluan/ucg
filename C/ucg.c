59e09dae1ad73ca36bb231f60719f4f17a9baa50
Cass <owlman@protonmail.com>
2024-08-11 21:47:50 +0100

    Moved all of the languages plus updated LICENSE
    - I opted to move all of the languages into their own folders. This makes sense as there were already folders before, like with the HTML and Minecraft port
    - I feel like it's going to be much simpler to just have `Copyright (c) 2021 - present` rather than citing all of the authors

---
+++ C/ucg.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(){puts("You are dying. 1=live");puts(getchar()==49?"won":"died");} 
--- ucg.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-#define x printf
-int main(){x("You are dying. 1=live ");if(getchar()==49){x("won");}else{x("died");}}
